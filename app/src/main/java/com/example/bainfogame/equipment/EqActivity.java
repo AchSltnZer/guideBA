022e3ba92682adb537ffcd3abe5ca8bd6ba90618
Ach Sltn <achsulton989@gmail.com>
2022-01-12 16:17:08 +0700

    update data

--- app/src/main/java/com/example/bainfogame/equipment/EqActivity.java
+++ app/src/main/java/com/example/bainfogame/equipment/EqActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.android.volley.Request;
